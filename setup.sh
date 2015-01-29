498220947357384a4642dfe5c60e7d93558c4238
danny crasto <danwald79@gmail.com>
2015-01-29 09:33:44 +0400

    added flake8 config and cleaned up setup, installing vundle

--- setup.sh
+++ setup.sh
@@ -1,36 +1,20 @@
 #!/usr/bin/env bash
 
-if [ "$(id -u)" != "0" ]; then
-    echo "This has to be run with sudo" 1>&2
-    exit 1
-fi
-
 set -e 
 DIR=`pwd -P $0`
 
-echo "Adding google ppa"
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-echo "updating apt"
-sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-
-echo "Updating packages .."
-apt-get update -qy > /dev/null
-echo "Installing packages .."
-apt-get install -qy curl vim zsh byobu git-core vim python-setuptools build-essential g++ ctags google-chrome-unstable ack-grep > /dev/null
-
 echo "Creating directories and links ... will overwrite"
 pushd $HOME > /dev/null
-mkdir -p ~/.vim/plugins ~/.vim/colors ~/.vim/autoload ~/.vim/bundle
+mkdir -p ~/.vim/plugins ~/.vim/colors ~/.vim/autoload ~/.vim/bundle ~/.config
 ln -sfv $DIR/.vimrc .vimrc
 ln -sfv $DIR/.screenrc .screenrc
 ln -sfv $DIR/.gitconfig .gitconfig
 ln -sfv $DIR/.zshrc .zshrc
 ln -sfv $DIR/.ackrc .ackrc
-chown -R $USER:$USER .vim .screenrc .vimrc
+ln -sfv $DIR/flake8 ~/.config/flake8
 
 echo "Installing zsh's pprompt .. "
 curl -LSs http://aperiodic.net/phil/prompt/prompt.txt -o .pprompt
-chown -R $USER:$USER .pprompt
 
 echo "Installing vim's xoria256 .. "
 curl -LSs https://raw2.github.com/vim-scripts/xoria256.vim/master/colors/xoria256.vim -o  .vim/colors/xoria256.vim
@@ -38,11 +22,8 @@ curl -LSs https://raw2.github.com/vim-scripts/xoria256.vim/master/colors/xoria25
 echo "Changing your shell to zsh .."
 chsh -s `which zsh` $USER
 
-echo "Installing vim's fugitive plugin .."
-pushd ~/.vim/bundle > /dev/null
-git clone http://github.com/tpope/vim-fugitive
-popd > /dev/null
-chown -R $USER:$USER /home/$USER
+echo "Installing vundle for your vim's plugin needs ..."
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 echo "All good in the hood. Re-login and don't overwrite the zsh config when prompted"
 popd > /dev/null
