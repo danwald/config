f51f786cd57a324b15e09d8b1808dec349279f14
danny crasto <danwald79@gmail.com>
2018-11-03 12:45:55 +0000

    Made sag script python2 compatible

--- bin/sag.py
+++ bin/sag.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
+
 import sys
 import os
 import platform
 import subprocess
+import tempfile
 
 try:
     from __futures__ import print_function
@@ -22,13 +24,27 @@ SSH_FILES = (
     'id_rsa_dubizzle_test',
 )
 
+def cmd_run(cmd):
+    output = ''
+    try:
+        result = subprocess.run(cmd)
+        if result.returncode:
+            output = result.stderr
+    except AttributeError:
+        with tempfile.TemporaryFile() as temp_file:
+            result = subprocess.call(cmd, stderr=temp_file)
+            if result:
+                temp_file.seek(0,0)
+                output = temp_file.readlines()
+    finally:
+        return output
 
 def run():
     for ssh_file in SSH_FILES:
         abs_ssh_file = os.path.join(SSH_HOME, ssh_file)
         cmd = "{cmd} {opts} {ssh_file}".format(cmd=SSH_ADD_CMD, opts=SSH_ADD_OPTS, ssh_file=abs_ssh_file).split(' ')
-        result = subprocess.run(cmd)
-        if result.returncode:
+        output = cmd_run(cmd)
+        if output:
             print('Failed Output:\n{err}'.format(err=result.stderr))
             break
 
