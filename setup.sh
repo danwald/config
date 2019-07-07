d49bc3104f61824cf2d222c11c4346eca117e096
danny crasto <danwald79@gmail.com>
2019-07-07 07:43:47 +0400

    dev null all excessive output

--- setup.sh
+++ setup.sh
@@ -22,11 +22,11 @@ PKGS="zsh curl git vim-runtime vim-gui-common"
 PKGS="$PKGS $PKG"
 
 echo "Updating your packages and install pre-requisites"
-$PKGMGR update
-$PKGMGR install zsh curl git vim-runtime vim-gui-common exuberant-ctags
+$PKGMGR update > /dev/null
+$PKGMGR install zsh curl git vim-runtime vim-gui-common exuberant-ctags > /dev/null
 
 echo "Install fzf"
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf > /dev/null
 
 echo "Creating directories and links ... will overwrite"
 pushd $HOME > /dev/null
@@ -43,7 +43,7 @@ ln -sfv $DIR/.hammerspoon .
 ln -sfv $DIR/bin/sag.py ~/bin/sag
 
 echo "Installing oh-my-zsh .. "
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh > /dev/null
 mkdir ~/.oh-my-zsh/custom/plugins/danwald/
 ln -sfv $DIR/danwald.plugin.zsh ~/.oh-my-zsh/custom/plugins/danwald/danwald.plugin.zsh
 
@@ -51,7 +51,7 @@ echo "Changing your shell to zsh .."
 chsh -s `which zsh` $USER
 
 echo "Installing vundle for your vim's plugin needs .."
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim > /dev/null
 
 echo "Installing google's python indent plugin"
 pushd $HOME/.vim/plugins > /dev/null
@@ -60,14 +60,14 @@ popd > /dev/null
 
 echo "Installing gpakosz's .tmux .."
 pushd $HOME > /dev/null
-git clone https://github.com/gpakosz/.tmux.git
+git clone https://github.com/gpakosz/.tmux.git > /dev/null
 ln -sfv .tmux/.tmux.conf
 cp -v .tmux/.tmux.conf.local .
 popd > /dev/null
 
 echo "Downloading Tmux plugin Manager"
 echo "Ammending tmux.local with TmuxPluginManager|tmux-resurrect|tmux|tmux-continuum"
-git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm > /dev/null
 cat $DIR/.danwald.tmux.local >> $HOME/.tmux.conf.local
 
 echo "All good in the hood. Re-login and don't overwrite the zsh config when prompted"
